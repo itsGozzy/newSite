4ab640866947293e1ed2d105b56811925d2a3a19
itsGozzy <gosdeng@gmail.com>
2017-06-17 03:21:44 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,10 @@
 
 $(function() {
 
-	// Vars.
 		var	$window = $(window),
 			$body = $('body'),
 			$wrapper = $('#wrapper');
 
-	// Breakpoints.
 		skel.breakpoints({
 			xlarge:	'(max-width: 1680px)',
 			large:	'(max-width: 1280px)',
@@ -15,14 +13,12 @@ $(function() {
 			xsmall:	'(max-width: 480px)'
 		});
 
-	// Disable animations/transitions until everything's loaded.
 		$body.addClass('is-loading');
 
 		$window.on('load', function() {
 			$body.removeClass('is-loading');
 		});
 
-	// Poptrox.
 		$window.on('load', function() {
 
 			$('.thumbnails').poptrox({
